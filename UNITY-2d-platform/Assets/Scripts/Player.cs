f947ddf308cb69511c96ab66469128c0d8e80463
JaimeA404 <jaimerafaavan@gmail.com>
2026-06-12 11:44:10 -0400

    added player left and right player movement i hope

--- UNITY-2d-platform/Assets/Scripts/Player.cs
+++ UNITY-2d-platform/Assets/Scripts/Player.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Rigidbody2D rb2d;
+    public Animator animator;
+    public float moveSpeedX = 1f;
+
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float moveX = Input.GetAxis("Horizontal");
+        if (Mathf.Abs(moveX) > 0.1f) 
+        {
+            float force = moveX * moveSpeedX;
+            rb2d.AddForceX(force, ForceMode2D.Force);
+        }
+        animator.SetFloat("moveSpeedX", Mathf.Abs(moveX));
+
     }
 }
